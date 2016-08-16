3ed9eae5f1d44d9fd171676db7b63ceabe351396
Mathias Brandt <maini123@gmail.com>
2016-08-16 13:00:15 +0200

    Implemented Facebook login with Firebase

--- FirebaseUIAuthTest/app/src/main/java/com/mathiasbrandt/firebaseuiauthtest/MainActivity.java
+++ FirebaseUIAuthTest/app/src/main/java/com/mathiasbrandt/firebaseuiauthtest/MainActivity.java
@@ -1,13 +1,79 @@
 package com.mathiasbrandt.firebaseuiauthtest;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import static com.firebase.ui.auth.ui.AcquireEmailHelper.RC_SIGN_IN;
 
 public class MainActivity extends AppCompatActivity {
+    public static String TAG = "MainActivity";
+    Button btnLogin;
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogout = (Button) findViewById(R.id.btn_logout);
+        btnLogout.setEnabled(false);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser() != null) {
+            Log.d(TAG, "Already logged in");
+            onLogin();
+        } else {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setProviders(AuthUI.FACEBOOK_PROVIDER)
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+    public void btnLogoutOnClick(View v) {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d(TAG, "User signed out");
+
+                        btnLogout.setEnabled(false);
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN) {
+            if(resultCode == RESULT_OK) {
+                Log.d(TAG, "User signed in");
+                onLogin();
+            } else {
+                Log.d(TAG, "User sign in failed");
+            }
+        }
+    }
+
+    private void onLogin() {
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText("Hello, " + FirebaseAuth.getInstance().getCurrentUser().getDisplayName() + "!");
+
+        btnLogout.setEnabled(true);
     }
 }
